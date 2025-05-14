8ea0f688e5a5f7b5418330500501a2fd1c15f6cd
Twiceurg <studies731@gmail.com>
2025-05-14 12:21:45 +0000

    ccorection du cron

--- cron/expirationCron.js
+++ cron/expirationCron.js
@@ -2,9 +2,10 @@ const cron = require("node-cron");
 const {
   AttributionDecision,
   NumeroAttribue,
-  AttributionNumero
+  AttributionNumero,UssdDecision, USSDAttribution, UssdAttribuer
 } = require("../models");
 const Sequelize = require("sequelize");
+const { Op } = require('sequelize');
 
 // Fonction pour vérifier les expirations et désactiver les décisions
 const verifierExpirationEtDesactiver = async () => {
@@ -68,8 +69,68 @@ const verifierExpirationEtDesactiver = async () => {
   }
 };
 
+const verifierUSSDExpirationEtDesactiver = async () => {
+  try {
+    const decisions = await UssdDecision.findAll({
+      where: {
+        date_expiration: {
+          [Op.ne]: null,
+          [Op.lt]: new Date()
+        },
+        etat_autorisation: true
+      },
+      include: [
+        {
+          model: USSDAttribution,
+          include: [
+            {
+              model: UssdAttribuer
+            }
+          ]
+        }
+      ]
+    });
+
+    for (let decision of decisions) {
+      console.log("Type de décision :", decision.type_decision);
+
+      if (decision.type_decision === "suspension") {
+        const attribution = decision.USSDAttribution;
+
+        if (attribution && attribution.UssdAttribuers?.length > 0) {
+          for (let numero of attribution.UssdAttribuers) {
+            console.log("Vérification du numéro USSD :", numero.ussd_attribue);
+
+            if (numero.statut === "suspendu") {
+              numero.statut = "attribue";
+              await numero.save();
+              console.log(
+                `Numéro ${numero.ussd_attribue} remis à 'attribue' suite à expiration de suspension.`
+              );
+            } else {
+              console.log(
+                `Le statut du numéro ${numero.ussd_attribue} est ${numero.statut}, aucune action.`
+              );
+            }
+          }
+        } else {
+          console.log(`Aucun numéro associé à l'attribution pour la décision ${decision.id}.`);
+        }
+      }
+
+      // Désactiver la décision
+      decision.etat_autorisation = false;
+      await decision.save();
+      console.log(`Décision ${decision.id} désactivée suite à expiration.`);
+    }
+  } catch (error) {
+    console.error("Erreur lors de la vérification des expirations USSD :", error);
+  }
+};
+
 // Cette tâche s'exécute tous les jours à minuit
 cron.schedule("0 0 * * *", () => {
   console.log("Vérification des expirations des décisions...");
   verifierExpirationEtDesactiver();
+  verifierUSSDExpirationEtDesactiver();
 });
